d8ea0c5f5e8068c67cb823103c8f9f7552b51947
Jonathan Horner <jonathanhorner10@gmail.com>
2017-10-01 16:30:13 +0100

    Added gtest CMake module.

---
+++ code/src/util/test/util_test.cpp
@@ -0,0 +1,19 @@
+#include "../util.hpp"
+
+#include <gtest/gtest.h>
+
+namespace fields {
+namespace util {
+namespace test {
+
+class UtilTest : public ::testing::Test {
+
+};
+
+TEST_F(UtilTest, BasicTest) {
+
+}
+
+}
+}
+}
