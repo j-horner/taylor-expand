799d256cac541298eb9a7699d77f524cb6e48ce9
Jonathan Horner <jonathanhorner10@gmail.com>
2018-01-23 21:42:00 +0000

    Added another literal test.

--- code/src/calculus/functions/test/constant_test.cpp
+++ code/src/calculus/functions/test/constant_test.cpp
@@ -17,18 +17,20 @@ TEST_F(ConstantTest, LiteralTest) {
     using namespace literals;
     using namespace operators;
 
-
     constexpr auto a = 123_c;
     constexpr auto b = 99999_c;
     constexpr auto c = 9223372036854775807_c;
     constexpr auto d = -9223372036854775807_c;
     constexpr auto e = 0_c;
 
+    constexpr auto f = a*4_c/2_c;
+
     static_assert(std::is_same_v<std::decay_t<decltype(a)>, Constant<123>>, "a is not Constant<123>");
     static_assert(std::is_same_v<std::decay_t<decltype(b)>, Constant<99999>>, "b is not Constant<99999>");
     static_assert(std::is_same_v<std::decay_t<decltype(c)>, Constant<9223372036854775807>>, "c is not Constant<9223372036854775807>");
     static_assert(std::is_same_v<std::decay_t<decltype(d)>, Constant<-9223372036854775807>>, "d is not Constant<-9223372036854775807>");
     static_assert(std::is_same_v<std::decay_t<decltype(e)>, Constant<0>>, "e is not Constant<0>");
+    static_assert(std::is_same_v<std::decay_t<decltype(f)>, Constant<246>>, "4*123/2 != 246");
 }
 
 }   // test
